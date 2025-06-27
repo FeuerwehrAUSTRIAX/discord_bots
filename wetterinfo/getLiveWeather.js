189786f28b9245a4b5a91a11f7a18cdb76e9d41c
FeuerwehrAUSTRIAX <feuerwehr.wienerneustadt.ax@gmail.com>
2025-06-27 16:57:13 +0200

    Update getLiveWeather.js

--- wetterinfo/getLiveWeather.js
+++ wetterinfo/getLiveWeather.js
@@ -7,7 +7,21 @@ async function getLiveWeather(city = 'Wien') {
     const data = res.data;
 
     const current = data.current_condition[0];
-    const condition = current.weatherDesc[0].value;
+    const conditionEN = current.weatherDesc[0].value;
+
+    // Optional: deutsche Übersetzung (einfach erweitern)
+    const conditionsMap = {
+      "Partly cloudy": "Teilweise bewölkt",
+      "Sunny": "Sonnig",
+      "Cloudy": "Bewölkt",
+      "Overcast": "Stark bewölkt",
+      "Patchy rain possible": "Leichter Regen möglich",
+      "Moderate rain": "Mäßiger Regen",
+      "Heavy rain": "Starker Regen",
+      "Thunderstorm": "Gewitter",
+    };
+    const condition = conditionsMap[conditionEN] || conditionEN;
+
     const temp = current.temp_C;
     const feelsLike = current.FeelsLikeC;
     const wind = current.windspeedKmph;
