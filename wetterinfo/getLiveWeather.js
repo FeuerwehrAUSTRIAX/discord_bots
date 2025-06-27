4ee398cf958879f2095056e789de470ccee84f69
FeuerwehrAUSTRIAX <feuerwehr.wienerneustadt.ax@gmail.com>
2025-06-27 16:19:25 +0200

    Update getLiveWeather.js

--- wetterinfo/getLiveWeather.js
+++ wetterinfo/getLiveWeather.js
@@ -1,1 +1,28 @@
+const axios = require('axios');
 
+async function getLiveWeather(city = 'Wien') {
+  try {
+    const url = `https://wttr.in/${encodeURIComponent(city)}?format=j1`;
+    const res = await axios.get(url);
+    const data = res.data;
+
+    const current = data.current_condition[0];
+    const condition = current.weatherDesc[0].value;
+    const temp = current.temp_C;
+    const feelsLike = current.FeelsLikeC;
+    const wind = current.windspeedKmph;
+
+    return {
+      city,
+      condition,
+      temp,
+      feelsLike,
+      wind,
+    };
+  } catch (err) {
+    console.error(`❌ Fehler beim Wetter für ${city}:`, err.message);
+    return null;
+  }
+}
+
+module.exports = getLiveWeather;
